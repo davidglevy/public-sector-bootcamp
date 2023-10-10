7a4e1979c1e1d7c2b2b02c66748d66d93df64ed1
david.g.levy@gmail.com <david.g.levy@gmail.com>
2023-10-10 01:28:27 +0000

    Finished introduction

--- 1. Introduction.py
+++ 1. Introduction.py
@@ -64,15 +64,6 @@ labContext.setupLab("introduction")
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC # Example Data Manipulation Language (DML) operations
-
-# COMMAND ----------
-
-# MAGIC %sql
-
-# COMMAND ----------
-
-# MAGIC %md
 # MAGIC ## Use Create-Table-As (CTAS) for Silver
 
 # COMMAND ----------
@@ -82,3 +73,66 @@ labContext.setupLab("introduction")
 # MAGIC AS
 # MAGIC SELECT id, to_date(dob_text, 'yyyy/MM/dd') AS dob, name, lower(fav_colour) AS fav_colour
 # MAGIC FROM people_bronze;
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Backfill Corrected Date-of-Birth columns to Silver
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CREATE OR REPLACE TABLE people_silver_backfill (
+# MAGIC   id bigint,
+# MAGIC   dob string
+# MAGIC );
+# MAGIC
+# MAGIC INSERT INTO people_silver_backfill
+# MAGIC (id, dob)
+# MAGIC VALUES
+# MAGIC (1, to_date('1982/01/05', 'yyyy/MM/dd')),
+# MAGIC (4, to_date('1988/10/11', 'yyyy/MM/dd')),
+# MAGIC (7, to_date('1990/03/20', 'yyyy/MM/dd'));
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC MERGE INTO people_silver p
+# MAGIC USING people_silver_backfill b
+# MAGIC ON p.id = b.id
+# MAGIC WHEN MATCHED THEN UPDATE SET p.dob = b.dob;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC DESCRIBE HISTORY people_silver;
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Lets Compare Old vs New Using Time Travel
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT p.id, p.name, p.dob AS dob_old, p2.dob AS dob_new
+# MAGIC FROM people_silver p
+# MAGIC INNER JOIN people_silver VERSION AS OF 0 AS p2 ON (p.id = p2.id)
+# MAGIC WHERE p.dob != p2.dob
+# MAGIC ORDER BY p.id;
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Create Example Gold Table
+# MAGIC We'll now create a basic gold table of aggregates
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CREATE TABLE people_names_gold
+# MAGIC AS
+# MAGIC SELECT name, count(*) AS `count`, min(year(dob)) AS first_born, max(year(dob)) AS last_born
+# MAGIC FROM people_silver
+# MAGIC GROUP BY name;
